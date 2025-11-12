c28a3c8dc70ffd227cd428604c484fcd23c763d0
De Villiers Neethling <devilliers.neethling@standardbank.co.za>
2025-11-12 21:59:50 +0200

    Val vs var

---
+++ Build Restful APIs using Kotlin SpringBoot/koltin-springboot/kotlin-playground/src/main/kotlin/com/kotlinplayground/basics/Variables.kt
@@ -0,0 +1,17 @@
+package com.kotlinplayground.basics
+
+fun main(){
+    // val = immutable
+    val name = "Beelzebub"
+    println(name)
+
+    // You can't do this here
+//    name = Azagthoth;
+
+    // var = variable, mutable
+    var age: Int = 37
+    println(age)
+    age = 38
+    println(age)
+
+}
