f61505e6ea637429484212bf71432e2eb0e7fd85
De Villiers Neethling <devilliers.neethling@standardbank.co.za>
2025-12-01 10:37:51 +0200

    Basic types

--- Build Restful APIs using Kotlin SpringBoot/koltin-springboot/kotlin-playground/src/main/kotlin/com/kotlinplayground/basics/Variables.kt
+++ Build Restful APIs using Kotlin SpringBoot/koltin-springboot/kotlin-playground/src/main/kotlin/com/kotlinplayground/basics/Variables.kt
@@ -1,8 +1,8 @@
 package com.kotlinplayground.basics
 
-fun main(){
+fun main() {
     // val = immutable
-    val name = "Beelzebub"
+    val name: String = "Beelzebub"
     println(name)
 
     // You can't do this here
@@ -13,5 +13,24 @@ fun main(){
     println(age)
     age = 38
     println(age)
+
+    // Kotlin type inference: it is not needed to declare the types explicitly
+    val salary: Long = 300000L
+    println(salary)
+
+    val course = "Kotlin Spring"
+    // String interpolation
+    println("course: $course")
+    // functions can also be executed here, eg
+    println("course: $course and the course length is ${course.length}")
+
+    val multiLine = "ABC \nDEF"
+    println(multiLine)
+
+    val fancyMultiLine = """
+        ABC
+        DEF
+    """.trimIndent()
+    println(fancyMultiLine)
 
 }
